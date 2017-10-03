d4653c378c5f58efeb00ea1d190f1d10a3671a05
pnmougel <pnmougel@gmail.com>
2017-10-03 10:19:57 +0200

    add trait for model with id

--- src/main/scala/org/lizeo/backend/domains/dealers/DealerModel.scala
+++ src/main/scala/org/lizeo/backend/domains/dealers/DealerModel.scala
@@ -1,9 +1,11 @@
 package org.lizeo.backend.domains.dealers
 
+import org.lizeo.backend.core.elasticsearch.EntityWithId
+
 /**
   * Created by nico on 30/09/17.
   */
 case class DealerModel(
   id: String,
   name: String,
-  dealerType: Int)
+  dealerType: Int) extends EntityWithId
--- src/main/scala/org/lizeo/backend/domains/stations/StationModel.scala
+++ src/main/scala/org/lizeo/backend/domains/stations/StationModel.scala
@@ -1,5 +1,7 @@
 package org.lizeo.backend.domains.stations
 
+import org.lizeo.backend.core.elasticsearch.EntityWithId
+
 /**
   * Created by nico on 30/09/17.
   */
@@ -12,4 +14,4 @@ case class StationModel (
   longitude: Double,
   address: String,
   postalCode: String,
-  city: String)
+  city: String) extends EntityWithId
